dbed780b8f6b29d96381442847c12e1d05cb6b54
Iryna <iryna.markovich@vocovo.com>
2022-12-22 16:02:20 +0100

    fix types

--- src/app.types.ts
+++ src/app.types.ts
@@ -1,20 +1,9 @@
 import { IncomingMessage, ServerResponse } from 'http'
 
-export type IncomingRequest = IncomingMessage & { body?: object }
+export type Request = IncomingMessage & { body?: object }
 
-export type ServerResp = ServerResponse & {
-  send: (data: unknown) => void
+export type Response = ServerResponse & {
+  send?: (data: unknown) => void
 }
 
-export type RequestListener = (
-  req: IncomingMessage,
-  res: ServerResponse
-) => void
-
-export type SendCallback = {
-  send: (data: unknown) => void
-}
-
-export type Middleware = RequestListener & {
-  send: SendCallback
-}
+export type Middleware = (req: Request, res: Response) => void
